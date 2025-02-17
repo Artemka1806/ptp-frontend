ba82779a4ee239f96705adecfb5f89c4550daf8b
Artemka1806 <tyschenkoartem2580@gmail.com>
2025-02-17 20:15:34 +0200

    implement custom PWA install prompt with improved UI and event handling

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,60 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 import 'mdui/mdui.css'
 import FooterComponent from './components/FooterComponent.vue'
 import { RouterView } from 'vue-router'
 import { setColorScheme } from 'mdui/functions/setColorScheme.js'
+import 'mdui/components/button.js'
 
 setColorScheme('#78dc77')
-// This variable will save the event for later use.
-let deferredPrompt
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevents the default mini-infobar or install dialog from appearing on mobile
-  e.preventDefault()
-  // Save the event because you'll need to trigger it later.
-  deferredPrompt = e
-  // Show your customized install prompt for your PWA
-  // Your own UI doesn't have to be a single element, you
-  // can have buttons in different locations, or wait to prompt
-  // as part of a critical journey.
-  showInAppInstallPromotion()
+
+const deferredPrompt = ref(null)
+const showInstallButton = ref(false)
+
+onMounted(() => {
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault()
+    deferredPrompt.value = e
+    showInstallButton.value = true
+  })
 })
+
+function installPWA() {
+  if (!deferredPrompt.value) return
+  deferredPrompt.value.prompt()
+  deferredPrompt.value = null
+  showInstallButton.value = false
+}
 </script>
+
 <template>
   <main>
     <RouterView />
   </main>
-  <footer>
-    <FooterComponent />
-  </footer>
+  <FooterComponent />
+  <div class="background-container" v-if="showInstallButton">
+    <mdui-button full-width @click="installPWA" class="install-button"
+      >Встановити додаток</mdui-button
+    >
+  </div>
 </template>
+
+<style scoped>
+.background-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgb(var(--mdui-color-background-dark));
+  z-index: 1000;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 10px 20px;
+}
+
+.install-button {
+  margin-top: 50%;
+}
+</style>
