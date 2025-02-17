84b94259e9fadd48d3e18cea8700d0eb2ec0aa54
Artemka1806 <tyschenkoartem2580@gmail.com>
2025-02-17 21:46:02 +0200

    remove Ukrainian installation prompt from PWA install button

--- src/App.vue
+++ src/App.vue
@@ -13,30 +13,4 @@ setColorScheme('#78dc77')
     <RouterView />
   </main>
   <FooterComponent />
-  <div class="background-container" v-if="showInstallButton">
-    <h1>Щоб продовжити треба встановити додаток або додати сайт на початковий екран</h1>
-    <mdui-button full-width @click="installPWA" class="install-button"
-      >Встановити додаток</mdui-button
-    >
-  </div>
 </template>
-
-<style scoped>
-.background-container {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgb(var(--mdui-color-background-dark));
-  z-index: 1000;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 10px 20px;
-}
-
-.install-button {
-  margin-top: 50%;
-}
-</style>
